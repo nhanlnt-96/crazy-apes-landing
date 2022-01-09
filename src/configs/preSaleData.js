9dc13c2a5db1e1761e50eae4660ea6e150b486e5
nhanlnt-96 <nhanlnt@hotmail.com>
2022-01-09 11:43:10 +0700

    [master]: Change bg and img

--- src/configs/preSaleData.js
+++ src/configs/preSaleData.js
@@ -1,6 +1,8 @@
-import Img1 from '../assets/imgs/PreSale1.png';
-import Img2 from '../assets/imgs/PreSale2.png';
-import Img3 from '../assets/imgs/PreSale3.png';
-import Img4 from '../assets/imgs/PreSale4.png';
+// import Img1 from '../assets/imgs/PreSale1.png';
+// import Img2 from '../assets/imgs/PreSale2.png';
+// import Img3 from '../assets/imgs/PreSale3.png';
+// import Img4 from '../assets/imgs/PreSale4.png';
+import Shadow from '../assets/imgs/shadowApes.jpeg'
 
-export const preSaleData = [Img1, Img2, Img3, Img4];
+// export const preSaleData = [Img1, Img2, Img3, Img4];
+export const preSaleData = [Shadow, Shadow, Shadow, Shadow];
--- src/configs/specialApesData.js
+++ src/configs/specialApesData.js
@@ -1,7 +1,9 @@
-import Img1 from '../assets/imgs/Special1.png';
-import Img2 from '../assets/imgs/Special2.png';
-import Img3 from '../assets/imgs/Special3.png';
-import Img4 from '../assets/imgs/Special4.png';
-import Img5 from '../assets/imgs/Special5.png';
+// import Img1 from '../assets/imgs/Special1.png';
+// import Img2 from '../assets/imgs/Special2.png';
+// import Img3 from '../assets/imgs/Special3.png';
+// import Img4 from '../assets/imgs/Special4.png';
+// import Img5 from '../assets/imgs/Special5.png';
+import Shadow from '../assets/imgs/shadowApes.jpeg'
 
-export const specialApesData = [Img1, Img2, Img3, Img4, Img5];
+// export const specialApesData = [Img1, Img2, Img3, Img4, Img5];
+export const specialApesData = [Shadow, Shadow, Shadow, Shadow, Shadow];
